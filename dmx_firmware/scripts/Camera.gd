e5b9130d10e2f1ea7f8cd252b591cb87cc10d908
Thomas <thomas.mallet14@gmail.com>
2025-04-04 14:28:49 +0200

    D pour droite Q pour gauche (déplacements)

--- dmx_firmware/scripts/Camera.gd
+++ dmx_firmware/scripts/Camera.gd
@@ -8,6 +8,9 @@ const RAY_DISTANCE: float = 100
 @export var camera_rotate_speed: float = 0.5
 @export var camera_zoom_speed: float = 10
 
+#@export var speedz: float = 10.0
+@export var speedx: float = 2
+
 @export var camera_zoom_steps: int = 5
 @export var camera_zoom_min: float = 0.05
 @export var camera_zoom_max: float = 10
@@ -38,6 +41,19 @@ func _move(delta: float) -> void:
 			translate_object_local(v.normalized() * delta * move_speed)
 	
 	last_mouse_pos_move = get_viewport().get_mouse_position()
+	
+	#if Input.is_action_pressed("camera_move_forward"):  # Vérifie si "Z" est pressé
+		#var forward = -transform.basis.z  # Obtient la direction avant de la caméra
+		#position += forward * speedz * delta  # Déplace dans la direction avantz
+	#if Input.is_action_pressed("camera_move_backward"):  # Vérifie si "Z" est pressé
+		#var forward = transform.basis.z  # Obtient la direction avant de la caméra
+		#position += forward * speedz * delta
+	if Input.is_action_pressed("camera_move_right"):  # Vérifie si "Z" est pressé
+		var forward = transform.basis.x  # Obtient la direction avant de la caméra
+		position += forward * speedx * delta
+	if Input.is_action_pressed("camera_move_left"):  # Vérifie si "Z" est pressé
+		var forward = -transform.basis.x  # Obtient la direction avant de la caméra
+		position += forward * speedx * delta
 
 func _rotate(delta: float) -> void:
 	if mouse_pressed and !shift:
@@ -67,6 +83,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("camera_zoom_out"):
 		camera_zoom_target += 1
 	camera_zoom_target = clamp(camera_zoom_target, 0, camera_zoom_steps)
+
 
 func _zoom(delta: float) -> void:
 	var zoom_factor = camera_zoom_target / camera_zoom_steps
